aea3a7090e5480a79eaddc48c4dd149449d68bb5
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2026-06-01 15:28:50 -0700

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if not s or len(s) == 1:
+            return True
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alnum(s[l]):
+                l += 1
+            while l < r and not self.alnum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+        
+        return True
+
+
+    def alnum(self, c):
+        if not c:
+            return False
+        
+        if 65 <= ord(c) <= 90 or 97 <= ord(c) <= 122 or 48 <= ord(c) <= 57:
+            return True
+        
+        return False
